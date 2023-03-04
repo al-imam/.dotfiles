a456164226b207e06e7328f96c8d2d1cda66504f
al-imam <alimam01828@gmail.com>
2023-03-04 15:13:15 +0600

    extract file replacement location from drop.txt file

--- index.mjs
+++ index.mjs
@@ -11,5 +11,9 @@ const folders = (await $`ls -a`).stdout.split("\n").slice(2, -1);
 folders.map((n, i) => {
   within(async () => {
     cd(n);
+    const files = await globby(".", { dot: true });
+    const index = files.indexOf("drop.txt");
+    const { stdout: location } = await $`cat ${files.at(index)}`;
+    console.log(files, location);
   });
 });
