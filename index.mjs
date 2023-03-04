1c92a513ab1c833b54d3312e1b1b485dc8f9e124
al-imam <alimam01828@gmail.com>
2023-03-04 14:46:37 +0600

    remove most of the work im changing implementation

--- index.mjs
+++ index.mjs
@@ -1,24 +1,7 @@
 #!/usr/bin/env zx
 import "zx/globals";
-
-import { globby } from "globby";
 import lnk from "lnk";
 
 $.verbose = false;
 
 cd(`${__dirname}/src`);
-
-const files = await globby(["."], { dot: true });
-
-lnk(files[0], "home", { overwrite: true })
-  .then(() => console.log("done"))
-  .catch(async (e) => {
-    // if (files[0].includes("/")) {
-    //   const folderName = files[0].split("/")[0];
-    //   $``;
-    // }
-    await $`mv ${e.dest}{,.bak.${Math.random().toString().split(".")[1]}}`;
-    console.log(e.dest, e.path);
-  });
-
-echo(JSON.stringify(files, null, 4));
