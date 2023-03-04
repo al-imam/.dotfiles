661fc03da75ff70238fd76d4525b888385bd37a2
al-imam <alimam01828@gmail.com>
2023-03-04 15:47:08 +0600

    Fix bug i'm not sure why second proccess is not waiting

--- index.mjs
+++ index.mjs
@@ -8,17 +8,19 @@ cd(`${__dirname}/src`);
 
 const folders = (await $`ls -a`).stdout.split("\n").slice(2, -1);
 
-const arrayOfFiles = folders.reduce((accumulator, currentValue) => {
-  within(async () => {
-    cd(currentValue);
+const configurations = [];
+
+for (const name of folders)
+  await within(async () => {
+    cd(name);
     const files = await globby(".", { dot: true });
     const index = files.indexOf("drop.txt");
     const { stdout: location } = await $`cat ${files.at(index)}`;
-    accumulator[currentValue] = {
+    configurations.push({
       files: files.filter((e) => e !== "drop.txt"),
       location,
-    };
-    console.log(accumulator);
+      name,
+    });
   });
-  return accumulator;
-}, {});
+
+console.log(configurations);
