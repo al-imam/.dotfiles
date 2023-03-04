3cfe47b50209dbb94b78e89c76c864ef2f53ea7d
al-imam <alimam01828@gmail.com>
2023-03-04 12:59:28 +0600

    Its work but not how i expected

--- index.mjs
+++ index.mjs
@@ -1,10 +1,15 @@
 #!/usr/bin/env zx
 import "zx/globals";
+
 import { globby } from "globby";
+import lnk from "lnk";
+
 $.verbose = false;
 
 cd(`${__dirname}/src`);
 
 const files = await globby(["."], { dot: true });
+
+lnk(files[0], "home").then(() => console.log("done"));
 
 echo(JSON.stringify(files, null, 4));
