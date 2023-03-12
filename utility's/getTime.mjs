a83384a112b48a86c50a90ac2262b0713ae7a063
al-imam <alimam01828@gmail.com>
2023-03-13 04:16:35 +0600

    create file for getTime function to separate logic

---
+++ utility's/getTime.mjs
@@ -0,0 +1,19 @@
+const time = new Intl.DateTimeFormat("en", {
+  day: "2-digit",
+  second: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  minute: "2-digit",
+  hour: "2-digit",
+});
+
+function getTime() {
+  return time
+    .format(new Date())
+    .slice(0, -3)
+    .replaceAll("/", "-")
+    .replaceAll(":", "-")
+    .replaceAll(", ", "_");
+}
+
+export default getTime;
