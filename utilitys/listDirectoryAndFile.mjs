345da639d7dce6b7022e8cd6b9ac939682368732
al-imam <alimam01828@gmail.com>
2023-03-15 03:36:40 +0600

    return file after normalize

--- utilitys/listDirectoryAndFile.mjs
+++ utilitys/listDirectoryAndFile.mjs
@@ -1,6 +1,11 @@
+import { normalize } from "path";
+
 async function listDirectoryAndFile(path = "") {
   const { stdout: output } = await $`ls -a ${path}`;
-  return output.split("\n").slice(2, -1);
+  return output
+    .split("\n")
+    .slice(2, -1)
+    .map((p) => normalize(p));
 }
 
 export default listDirectoryAndFile;
