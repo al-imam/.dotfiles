8390c8fc6bfe31882908aacffd399059330821f5
al-imam <alimam01828@gmail.com>
2023-05-06 18:25:56 +0600

    destroy nested file symlink but dont change name

--- utilitys/util.mjs
+++ utilitys/util.mjs
@@ -1,28 +1,34 @@
 import getTime from "./getTime.mjs";
 import listDirectoryAndFile from "./listDirectoryAndFile.mjs";
-import { join } from "path";
+import { lstatSync } from "fs";
 
 const green = chalk.green;
 const yellow = chalk.yellow;
 
 async function recursiveBackup(path) {
   await within(async () => {
-    cd(path)
+    cd(path);
     const files = await listDirectoryAndFile();
     for (const file of files) {
-      if (fs.lstatSync(file).isDirectory()) {
-        recursiveBackup(file)
-        continue
-      };
-      await backupFile(file);
+      if (lstatSync(file).isDirectory()) {
+        recursiveBackup(file);
+        continue;
+      }
+      await backupFileDontChangeName(file);
     }
-  })
+  });
 }
 
 export async function backupFolder(item, backup = `${item}.bak_${getTime()}`) {
   await $`mv ${item} ${backup}`;
-  recursiveBackup(backup)
+  recursiveBackup(backup);
   return [item, backup];
+}
+
+async function backupFileDontChangeName(item) {
+  const { stdout: cat } = await $`cat ${item}`;
+  await $`rm --force ${item}`;
+  await $`echo ${cat} > ${item}`;
 }
 
 export async function backupFile(item, backup = `${item}.bak_${getTime()}`) {
@@ -33,5 +39,5 @@ export async function backupFile(item, backup = `${item}.bak_${getTime()}`) {
 
 export function showLogs(x) {
   const [file, backup] = x;
-  return yellow(`♻️ ${file} ${green("->")} ${backup}`);
+  return yellow(`♻️  ${file} ${green("->")} ${backup}`);
 }
