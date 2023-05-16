a291c17bf1b0bec33393b2d71c568ebad33e5abe
al-imam <alimam01828@gmail.com>
2023-05-16 20:09:28 +0600

    setup all config in getConfig

--- utilitys/getConfig.mjs
+++ utilitys/getConfig.mjs
@@ -1,3 +1,15 @@
-export default ({ y = false, b = false }) => {
-  return { yes: y, backup: !b };
-};
+function getConfig({ y = false, b = false }) {
+  return () => ({
+    yes: y,
+    backup: !b,
+    yellow: chalk.yellow,
+    red: chalk.red,
+    green: chalk.green,
+    dim: chalk.dim,
+    blue: chalk.blue,
+    purple: chalk.hex("#ff92df"),
+    blueLight: chalk.hex("#aa77ff"),
+  });
+}
+
+export default getConfig();
